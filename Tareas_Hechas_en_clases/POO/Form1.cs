c532e731683498faab26372d5d7558419220d806
Jaaq28 <90659536+Jaaq28@users.noreply.github.com>
2021-09-28 14:53:37 -0600

    Class Usuario
    definicion de atributos,propiedades metodos.

---
+++ Tareas_Hechas_en_clases/POO/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace POO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Usuario usuario = new Usuario();
+
+            usuario.Codigo = "NACASCO";
+            usuario.Correo = "nacasco@poo.com";
+            usuario.Nombre = "NAUM";
+            usuario.Apellidos = "CASCO";
+
+            MessageBox.Show(usuario.GetNombreCompleto(usuario.Nombre, usuario.Apellidos));
+        }
+    }
+}
---
+++ Tareas_Hechas_en_clases/POO/Usuario.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO
+{
+    public class Usuario
+    {
+        //propiedades,caracteristicas
+
+
+
+        //Atributos
+        private string apellido;
+
+
+        //propiedades
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Apellidos
+        {
+            set { apellido = value; }
+
+            get { return apellido; }
+
+        }
+
+        public int Edad { get; set; }
+        public string Correo { get; set; }
+        public string Contrasena { get; set; }
+
+        public Usuario()
+        {
+        }
+
+        public Usuario(string codigo, string correo)
+        {
+            Codigo = codigo;
+            Correo = correo;
+        }
+
+        public Usuario(string codigo, string nombre, string apellidos, int edad, string correo, string contrasena) : this(codigo, nombre)
+        {
+            Apellidos = apellidos;
+            Edad = edad;
+            Correo = correo;
+            Contrasena = contrasena;
+        }
+
+        //metodos
+
+        public string GetNombreCompleto(string nombres, string apellidos)
+        {
+            string _NombreCompleto = nombres + "" + apellidos;
+            return _NombreCompleto;
+        }
+    }
+}
