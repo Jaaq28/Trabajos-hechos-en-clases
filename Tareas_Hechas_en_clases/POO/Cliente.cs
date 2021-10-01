caa23e2c4443191386b45d8f29a93d865334657b
Jaaq28 <90659536+Jaaq28@users.noreply.github.com>
2021-10-01 13:06:57 -0600

    Ejercico hecho por el lic

---
+++ Tareas_Hechas_en_clases/POO/Cliente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO
+{
+    public class Cliente
+    {
+
+        public string Identidad { get; set; }
+
+        public string Nombre { get; set; }
+
+        public Cliente (string identidad, string nombre) 
+        {
+
+        }
+    }
+}
---
+++ Tareas_Hechas_en_clases/POO/MovimientosCuenta.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO
+{
+   public class MovimientosCuenta
+    {
+        public Cuenta MiCuenta { get; set; }
+        DateTime FechaMovimiento { get; set; }
+
+        public decimal Monto { get; set; }
+
+        public string TipoMovimiento { get; set; }
+
+        public MovimientosCuenta() { }
+
+        public MovimientosCuenta(Cuenta miCuenta, DateTime fechaMovimiento, decimal monto, string tipoMovimiento)
+        {
+            MiCuenta = miCuenta;
+            FechaMovimiento = fechaMovimiento;
+            Monto = monto;
+            TipoMovimiento = tipoMovimiento;
+        }
+    }
+}
